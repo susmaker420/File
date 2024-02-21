9436f8bc96439f284913eeb0e94e3c694a7b1dda
susmaker420 <alexander.trifunovic@elev.ga.ntig.se>
2024-02-21 11:05:13 +0100

    början av koden

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
-
-    public class Start{
-    static Val val = new Val();
-    }
     public static void main(String[] args) {
+        Val val = new Val();
         val.choose();                        // ska till kalla val
     }
 }
